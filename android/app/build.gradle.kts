df8a7779494562c1f3d0b1679036a49a3abb46f3
TPhimX Developer <tphimx@example.com>
2026-06-14 13:00:28 +0700

    A

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,13 +10,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = "19"
+        jvmTarget = "17"
     }
 
     defaultConfig {
